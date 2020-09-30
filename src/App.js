21c0d3b1332c86df6c2121f3178e0a7d9bb6a67f
= <=>
2020-10-01 00:59:35 +1000

    Dashboard page with ticket component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import { DefaultLayout } from './layout/DefaultLayout';
+import { Dashboard } from './pages/dashboard/Dashboard.page';
 
 function App() {
   return (
     <div className="App">
       <DefaultLayout>
-        Component
+        <Dashboard />
       </DefaultLayout>
     </div>
   );
---
+++ src/pages/dashboard/Dashboard.page.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { TicketTable } from '../../components/ticket-table/TicketTable.comp';
+import tickets from '../../assets/data/dummy-tickets.json';
+
+export const Dashboard = () => {
+  return (
+    <Container>
+      <Row>
+        <Col className="text-center mt-5 mb-2">
+          <Button variant="info" style={{ fontSize: "2rem", padding: "10px 30px" }}>Add New Ticket</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="text-center mb-2">
+          <div>Total tickets: 50</div>
+          <div>Pending tickets: 5</div>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="mb-2">
+          Recently Added Tickets
+        </Col>
+      </Row>
+      <hr />
+      <Row>
+        <Col className="recent-tickets">
+          <TicketTable tickets={tickets} />
+        </Col>
+      </Row>
+    </Container>
+  )
+}
