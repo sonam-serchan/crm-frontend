a377f654b2608824bc12eef589689e38545c2378
= <=>
2020-10-02 01:37:20 +1000

    add new ticket page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import { DefaultLayout } from './layout/DefaultLayout';
-import { Dashboard } from './pages/dashboard/Dashboard.page';
+// import { Dashboard } from './pages/dashboard/Dashboard.page';
+import { AddTicket } from './pages/new-ticket/AddTicket.page';
 
 function App() {
   return (
     <div className="App">
       <DefaultLayout>
-        <Dashboard />
+        {/* <Dashboard /> */}
+        <AddTicket />
       </DefaultLayout>
     </div>
   );
--- src/components/breadcrumb/Breadcrumb.comp.js
+++ src/components/breadcrumb/Breadcrumb.comp.js
@@ -3,7 +3,7 @@ import { Breadcrumb } from 'react-bootstrap';
 
 export const PageBreadcrumb = ({ page }) => {
   return (
-    <Breadcrumb>
+    <Breadcrumb className="mt-3">
       <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
       <Breadcrumb.Item active>{page}</Breadcrumb.Item>
     </Breadcrumb>
