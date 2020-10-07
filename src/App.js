b9a236c414effd6cd1e73eff0cddc3c278d69cfb
= <=>
2020-10-07 23:03:47 +1100

    tickets list page added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 import { DefaultLayout } from './layout/DefaultLayout';
 // import { Dashboard } from './pages/dashboard/Dashboard.page';
-import { AddTicket } from './pages/new-ticket/AddTicket.page';
+// import { AddTicket } from './pages/new-ticket/AddTicket.page';
+import { TicketLists } from './pages/ticket-list/TicketLists.page';
 
 function App() {
   return (
     <div className="App">
       <DefaultLayout>
         {/* <Dashboard /> */}
-        <AddTicket />
+        {/* <AddTicket /> */}
+        <TicketLists />
       </DefaultLayout>
     </div>
   );
---
+++ src/pages/ticket-list/TicketLists.page.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { PageBreadcrumb } from '../../components/breadcrumb/Breadcrumb.comp';
+import { SearchForm } from '../../components/search-form/SearchForm.comp';
+import { TicketTable } from '../../components/ticket-table/TicketTable.comp';
+import tickets from '../../assets/data/dummy-tickets.json';
+
+export const TicketLists = () => {
+  const [str, setStr] = useState('');
+  const [ticket, setTicket] = useState(tickets);
+
+  const handleOnChange = e => {
+    const { value } = e.target;
+    setStr(value);
+    searchTicket(value);
+  }
+
+  const searchTicket = (sub) => {
+    const filteredTickets = tickets.filter(row => row.subject.toLowerCase().includes(sub.toLowerCase()));
+    setTicket(filteredTickets);
+  }
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <PageBreadcrumb page="Ticket Lists" />
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        <Col>
+          <Button variant="info">Add New Ticket</Button>
+        </Col>
+        <Col className="text-right">
+          <SearchForm handleOnChange={handleOnChange} str={str} />
+        </Col>
+      </Row>
+      <hr />
+      <Row>
+        <Col>
+          <TicketTable tickets={ticket} />
+        </Col>
+      </Row>
+    </Container>
+  )
+}
